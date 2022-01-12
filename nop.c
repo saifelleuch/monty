c2e5deacacf7079189989313eb05b1b463da97e3
saifelleuch <sa2014if@gmail.com>
2022-01-12 13:41:14 -0800

    nop func

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything.
+ * @stack: the header of the stack
+ * @line_number: the line number
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
