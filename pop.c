29b0089ce4efbdadd48bde3ba44e6a8f83b50c9c
saifelleuch <sa2014if@gmail.com>
2022-01-12 13:31:52 -0800

    pop func

---
+++ pop.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * pop - emoves the top element of the stack.
+ * @stack: the header of the stack
+ * @line_number: the line number
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	temp = *stack;
+	if (*stack == NULL)
+	{
+		dprintf(STDOUT_FILENO, "L%u: can't pop an empty stack", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (*stack != NULL)
+	{
+		*stack = *stack->next;
+		free(temp);
+	}
+
+	if (*stack->next == NULL && *stack->prev == NULL)
+	{
+		free(stack);
+	}
+
+}
